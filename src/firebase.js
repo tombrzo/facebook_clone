73406517054bbe4e2629ada4715cf5e8d9984af6
tomasz brzoza <tombrzoza50@gmail.com>
2021-04-19 21:47:13 +0200

    first commit add header

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhwbMpFZAmyNkvH-cvAxzQUya8JhuOfCU",
+  authDomain: "facebook-clone-da2ac.firebaseapp.com",
+  projectId: "facebook-clone-da2ac",
+  storageBucket: "facebook-clone-da2ac.appspot.com",
+  messagingSenderId: "647155992594",
+  appId: "1:647155992594:web:4b3ffa6e7dbf2edab430cb",
+  measurementId: "G-20SL9C6077"
+};
+
+// databaseURL: 'https://facebook-clone-da2ac.firebaseio.com',
